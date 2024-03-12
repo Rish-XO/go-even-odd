c831ef19cbbd4efff7e8e5cd1299afdfde97d5f5
Rish-XO <rishal.mhd009@gmail.com>
2024-03-12 16:15:28 +0530

    sameple

--- main.go
+++ main.go
@@ -7,7 +7,9 @@ func main() {
 
 	for _, number := range numbers {
 		if number%2 == 0 {
-			fmt.Println()
+			fmt.Println(number, " is even")
+		} else{
+			fmt.Println(number, " is odd")
 		}
 	}
 }
