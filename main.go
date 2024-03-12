45f8159a884643692e5136f1f8123d0e6e2fda1b
Rish-XO <rishal.mhd009@gmail.com>
2024-03-12 16:13:25 +0530

    kdjfds

--- main.go
+++ main.go
@@ -1,8 +1,13 @@
 package main
 
+import "fmt"
 
 func main() {
-	numbers := []int{0,1,2,3,4,5,6,7,8,9,10}
-	
-	for index
+	numbers := []int{0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
+
+	for _, number := range numbers {
+		if number%2 == 0 {
+			fmt.Println()
+		}
+	}
 }
